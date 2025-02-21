8db75715c796201b5aa88f4e460867cc5d97688b
Filip Tanurovski <filip@tanurov.ski>
2025-02-21 10:31:28 +0100

    feat: add uvicorn

--- aws/ec2/service.sh
+++ aws/ec2/service.sh
@@ -1,5 +1,8 @@
 cd /home/ubuntu/color-rp
+git pull
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
-python3 colorai/manage.py runserver 0.0.0.0:8000 --noreload
+export PYTHONUNBUFFERED=1
+export DJANGO_SETTINGS_MODULE=colorai.settings
+uvicorn colorai.asgi:application --host 0.0.0.0 --port 8000 --workers 4
