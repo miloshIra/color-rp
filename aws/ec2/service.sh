4f60a8e377c1f01bd001d83179846c01288cc290
Filip Tanurovski <filip@tanurov.ski>
2025-02-21 10:12:43 +0100

    fix: update service script and allowed hosts

--- aws/ec2/service.sh
+++ aws/ec2/service.sh
@@ -2,4 +2,4 @@ cd /home/ubuntu/color-rp
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
-python3 colorai/manage.py runserver
+python3 colorai/manage.py runserver 0.0.0.0:8000 --noreload
