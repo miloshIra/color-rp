839ac07235a87f897822d96871917dfee02eec3e
Filip Tanurovski <filip@tanurov.ski>
2025-02-21 14:06:42 +0100

    chore: cleanup

--- aws/ec2/service.sh
+++ aws/ec2/service.sh
@@ -3,6 +3,7 @@ git pull
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
+cd colorai
 export PYTHONUNBUFFERED=1
 export DJANGO_SETTINGS_MODULE=colorai.settings
 uvicorn colorai.asgi:application --host 0.0.0.0 --port 8000 --workers 4
